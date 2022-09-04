65948488a81b965388f207f6a7fb7689d5e2de4e
euFilpeSilva <sousadasilvafilipe@gmail.com>
2022-09-04 14:09:11 -0300

    implementando gerenciamento de estado pra rmazenar novos estudantes em um array

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,19 +3,37 @@ import React, { useState } from 'react';
 import './style.css'
 
 export default function Home() {
-  const [studantName, setStudantName] = useState('')
+  const [studentName, setStudantName] = useState('')
+  const [students, setStudents] = useState([])
 
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString('pt-br', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      })
+    };
+
+    setStudents([newStudent])
+  }
 
   return (
     <div className="container">
-      <h1>Nome: { studantName }</h1>
+      <h1>Nome: { studentName }</h1>
       <input type="text"
        placeholder="Digite o nome..."
        onChange={e => setStudantName(e.target.value)}></input>
 
-      <button type="button">Adicionar</button>
-      <Card name="Filipe" time=" 11:30:05" />
-      <Card name="Rodrigo" time=" 10:20:35" />
+      <button 
+      type="button"
+      onClick={handleAddStudent}
+      >Adicionar</button>
+      {
+        students.map(student => <Card name={student.name} time={students.time} /> ) 
+      }
+      
       
     </div>
   )
