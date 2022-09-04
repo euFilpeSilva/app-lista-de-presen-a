e7543541e3bae523e0bf70e689ab5ea6cea17bb0
euFilpeSilva <sousadasilvafilipe@gmail.com>
2022-09-04 14:52:30 -0300

    implementando ascincronismo junto com useEffect

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,11 @@
 import Card from '../../components/Card';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css'
 
 export default function Home() {
   const [studentName, setStudantName] = useState('')
   const [students, setStudents] = useState([])
+  const [user, setUser] = useState({ name: '', avatar: ''})
 
   function handleAddStudent() {
     const newStudent = {
@@ -19,9 +20,27 @@ export default function Home() {
     setStudents(prevState => [...prevState, newStudent])
   }
 
+  useEffect(() => { 
+    async function fetchData() {
+     const response = await fetch('https://api.github.com/users/euFilpeSilva')
+      const data = await response.json();
+        setUser({
+          name: data.name,
+          avatar: data.avatar_url
+        });  
+    }
+    fetchData();
+  },[])
+
   return (
     <div className="container">
-      <h1>Nome: { studentName }</h1>
+      <header>
+        <h1>Lista de presença</h1>
+        <div>
+            <strong>{user.name}</strong>
+            <img src={user.avatar} alt="Foto de perfil"></img>
+        </div>
+      </header>
       <input type="text"
        placeholder="Digite o nome..."
        onChange={e => setStudantName(e.target.value)}></input>
