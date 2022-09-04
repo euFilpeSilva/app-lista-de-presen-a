68f190720518c4174df09fdf2c2b5b7c4c127152
euFilpeSilva <sousadasilvafilipe@gmail.com>
2022-09-04 12:44:47 -0300

    estruturando projeto e criando primeiras estilizações

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import './Home.css'
+
+export default function Home() {
+
+  return (
+    <div className="container">
+      <h1>Lista de presença</h1>
+      <input type="text" placeholder="Digite o nome..."></input>
+      <button type="button">Adicionar</button>
+    </div>
+  )
+}
+
+
